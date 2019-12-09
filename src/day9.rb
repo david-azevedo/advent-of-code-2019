3fa9994e89a0e297447a657039d6a1110c82b244
David Azevedo <davidazevedo1996@hotmail.com>
2019-12-09 13:57:39 +0000

    WIP Day 9 Part 1

---
+++ src/day9.rb
@@ -0,0 +1,153 @@
+def add(code, arr, pos1, pos2, pos3)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  val_2 = convert_mode(code / 1000 % 10, arr, pos2)
+  val_3 = convert_mode(code / 10_000 % 10, arr, pos3)
+
+  arr[val_3.abs] = val_1 + val_2
+end
+
+def multiply(code, arr, pos1, pos2, pos3)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  val_2 = convert_mode(code / 1000 % 10, arr, pos2)
+  val_3 = convert_mode(code / 10_000 % 10, arr, pos3)
+
+  arr[val_3.abs] = val_1 * val_2
+end
+
+def consume_input(code, arr, pos1)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  
+  arr[val_1.abs] = $input
+end
+
+def write_to_output(code, arr, pos1)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  
+  mode = code / 100 % 10
+
+  if mode == 1
+    $input = val_1
+  else
+    $input = arr[val_1.abs] || 0
+  end
+  puts "output: #{$input}"
+end
+
+def jump_if_true(code, arr, pos1, pos2)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  val_2 = convert_mode(code / 1000 % 10, arr, pos2)
+  [val_1 != 0, val_2]
+end
+
+def jump_if_false(code, arr, pos1, pos2)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  val_2 = convert_mode(code / 1000 % 10, arr, pos2)
+  [val_1.zero?, val_2]
+end
+
+def less_than(code, arr, pos1, pos2, pos3)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  val_2 = convert_mode(code / 1000 % 10, arr, pos2)
+  val_3 = convert_mode(code / 10_000 % 10, arr, pos3)
+
+  arr[val_3.abs] = val_1 < val_2 ? 1 : 0
+end
+
+def equals(code, arr, pos1, pos2, pos3)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+  val_2 = convert_mode(code / 1000 % 10, arr, pos2)
+  val_3 = convert_mode(code / 10_000 % 10, arr, pos3)
+
+  arr[val_3.abs] = val_1 == val_2 ? 1 : 0
+end
+
+def add_to_relative_base(code, arr, pos1)
+  val_1 = convert_mode(code / 100 % 10, arr, pos1)
+
+  mode = code / 100 % 10
+  if mode == 1
+    $relative_base += val_1
+  else
+    $relative_base += arr[val_1.abs] || 0
+  end
+end
+
+
+def convert_mode(mode, arr, pos)
+  case mode
+  when 0
+    arr[pos.abs] || 0
+  when 1
+    pos
+  when 2
+    arr[($relative_base + pos).abs] || 0
+  else
+    puts 'Ooops'
+  end
+end
+
+def reset_memory
+  File.readlines('../data/day9.txt')
+end
+
+# Change this to regular for loop
+def compute(array)
+
+  index = 0
+  while index < array.length
+    code = array[index]
+    instruction = code % 100
+    #puts "instruction: #{instruction}"
+    #puts "array: #{array.join(',')}"
+    case instruction
+    when 1
+      add(code, array, array[index + 1], array[index + 2], array[index + 3])
+      index += 4
+    when 2
+      multiply(code, array, array[index + 1], array[index + 2], array[index + 3])
+      index += 4
+    when 3
+      # Input
+      consume_input(code, array, array[index + 1])
+      index += 2
+    when 4
+      # Output
+      write_to_output(code, array, array[index + 1])
+      index += 2
+    when 5
+      bool, new_index = jump_if_true(code, array, array[index + 1], array[index + 2])
+
+      index = bool ? new_index : index + 3
+    when 6
+      bool, new_index = jump_if_false(code, array, array[index + 1], array[index + 2])
+
+      index = bool ? new_index : index + 3
+    when 7
+      less_than(code, array, array[index + 1], array[index + 2], array[index + 3])
+      index += 4
+    when 8
+      equals(code, array, array[index + 1], array[index + 2], array[index + 3])
+      index += 4
+    when 9
+      add_to_relative_base(code, array, array[index + 1])
+      index += 2
+    when 99
+      break
+    else
+      break
+    end
+  end
+end
+
+def reset_globals
+  $input = 1
+  $relative_base = 0
+end
+
+arrays = reset_memory
+
+arrays.each_with_index do |array, index|
+  reset_globals
+  puts "Input ##{index}"
+  compute(array.split(',').map(&:to_i))
+end
