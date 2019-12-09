91b3a8f238e1abc6e27cc2a600903dfbc0257202
David Azevedo <davidazevedo1996@hotmail.com>
2019-12-09 16:55:15 +0000

    Day 9 Refactor

--- src/day9.rb
+++ src/day9.rb
@@ -1,30 +1,28 @@
 def add(code, arr, pos1, pos2, pos3)
   val_1 = convert_mode(code / 100 % 10, arr, pos1)
   val_2 = convert_mode(code / 1000 % 10, arr, pos2)
-  val_3 = convert_literal(code / 10_000 % 10, arr, pos3)
+  val_3 = convert_literal(code / 10_000 % 10, pos3)
 
-
-  arr[val_3] = val_1 + val_2 if val_3 >= 0
+  arr[val_3] = val_1 + val_2
 end
 
 def multiply(code, arr, pos1, pos2, pos3)
   val_1 = convert_mode(code / 100 % 10, arr, pos1)
   val_2 = convert_mode(code / 1000 % 10, arr, pos2)
-  val_3 = convert_literal(code / 10_000 % 10, arr, pos3)
+  val_3 = convert_literal(code / 10_000 % 10, pos3)
 
-  arr[val_3] = val_1 * val_2 if val_3 >= 0
+  arr[val_3] = val_1 * val_2
 end
 
 def consume_input(code, arr, pos1)
-  val_1 = convert_literal(code / 100 % 10, arr, pos1)
-  
-  puts "Input: #{$input}"
+  val_1 = convert_literal(code / 100 % 10, pos1)
+
   arr[val_1] = $input
 end
 
 def write_to_output(code, arr, pos1)
   val_1 = convert_mode(code / 100 % 10, arr, pos1)
-  
+
   $input = val_1
   puts "output: #{$input}"
 end
@@ -44,17 +42,17 @@ end
 def less_than(code, arr, pos1, pos2, pos3)
   val_1 = convert_mode(code / 100 % 10, arr, pos1)
   val_2 = convert_mode(code / 1000 % 10, arr, pos2)
-  val_3 = convert_literal(code / 10_000 % 10, arr, pos3)
+  val_3 = convert_literal(code / 10_000 % 10, pos3)
 
-  arr[val_3] = val_1 < val_2 ? 1 : 0 if val_3 >= 0
+  arr[val_3] = val_1 < val_2 ? 1 : 0
 end
 
 def equals(code, arr, pos1, pos2, pos3)
   val_1 = convert_mode(code / 100 % 10, arr, pos1)
   val_2 = convert_mode(code / 1000 % 10, arr, pos2)
-  val_3 = convert_literal(code / 10_000 % 10, arr, pos3)
+  val_3 = convert_literal(code / 10_000 % 10, pos3)
 
-  arr[val_3] = val_1 == val_2 ? 1 : 0 if val_3 >= 0
+  arr[val_3] = val_1 == val_2 ? 1 : 0
 end
 
 def add_to_relative_base(code, arr, pos1)
@@ -63,19 +61,18 @@ def add_to_relative_base(code, arr, pos1)
   $relative_base += val_1
 end
 
-
 def convert_mode(mode, arr, pos)
   case mode
   when 0
-    arr[pos] || 0 if pos >= 0
+    arr[pos] || 0
   when 1
     pos
   when 2
-    arr[$relative_base + pos] || 0 if $relative_base + pos >= 0
+    arr[$relative_base + pos] || 0
   end
 end
 
-def convert_literal(mode, arr, pos)
+def convert_literal(mode, pos)
   case mode
   when 0
     pos
@@ -87,7 +84,7 @@ def convert_literal(mode, arr, pos)
 end
 
 def reset_memory
-  File.readlines('../data/day9.txt')
+  File.read('../data/day9.txt').split(',').map(&:to_i)
 end
 
 # Change this to regular for loop
@@ -97,9 +94,6 @@ def compute(array)
   while index < array.length
     code = array[index]
     instruction = code % 100
-
-    #puts "instruction: #{instruction}"
-    #puts "array: #{array.join(',')}"
     case instruction
     when 1
       add(code, array, array[index + 1], array[index + 2], array[index + 3])
@@ -141,15 +135,10 @@ def compute(array)
 end
 
 def reset_globals
-  $input = 2
+  $input = 1
   $relative_base = 0
 end
 
-arrays = reset_memory
-
-arrays.each_with_index do |array, index|
-  reset_globals
-  puts "Input ##{index}"
-  array_int = array.split(',').map(&:to_i)
-  compute(array_int)
-end
+reset_globals
+array = reset_memory
+compute(array)
